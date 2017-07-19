37dff843f0b65e20c3b4b310e8f07b3f0711dc2f
Yu Jian <jdyujian@126.com>
2017-07-19 23:39:52 +0800

    3-4

--- message/models.py
+++ message/models.py
@@ -1,12 +1,15 @@
+# _*_ coding:utf-8 _*_
 from django.db import models
 
 # Create your models here.
 class UserMessage(models.Model):
-    name = models.CharField(max_length=20, verbose_name=u"Username")
+    object_id = models.CharField(primary_key=True,default="", verbose_name="MainKey", max_length=50)
+    name = models.CharField(max_length=20, verbose_name=u"Username", null=True, blank=True, default="")
     email = models.EmailField(verbose_name=u"emailaddress")
     address = models.CharField(max_length=100, verbose_name=u"address")
     message = models.CharField(max_length=500, verbose_name=u"information")
 
     class Meta:
-        verbose_name = u"user message"
+        verbose_name = u"User Message"
+        verbose_name_plural = verbose_name
 
