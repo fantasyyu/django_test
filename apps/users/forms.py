4d1f64f0863b1544678c4f005544ea9e69ee4aab
Yu Jian <jdyujian@126.com>
2017-07-30 14:51:13 +0800

    6-5

---
+++ apps/users/forms.py
@@ -0,0 +1,8 @@
+# _*_ coding:utf-8 _*_
+
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, min_length=4)
+
