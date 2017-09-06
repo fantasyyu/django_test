cfac88e9c0403db79003c86af0eb3eec19b9bde4
Yu Jian <jdyujian@126.com>
2017-09-06 20:55:38 +0800

    13-4

--- apps/courses/adminx.py
+++ apps/courses/adminx.py
@@ -2,7 +2,17 @@
 
 import xadmin
 
-from .models import Course, Lesson, Video, CourseResource
+from .models import Course, Lesson, Video, CourseResource, BannerCourse
+
+
+class LessonInline(object):
+    model = Lesson
+    extra = 0
+
+
+class CourseResourceInline(object):
+    model = CourseResource
+    extra = 0
 
 class CourseAdmin(object):
     list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums']
@@ -11,16 +21,40 @@ class CourseAdmin(object):
     ordering = ['-click_nums']
     readonly_fields = ['fav_nums', 'students']
     exclude = ['click_nums']
+    inlines = [LessonInline, CourseResourceInline]
+
+    def queryset(self):
+        qs = super(CourseAdmin, self).queryset()
+        qs.filter(is_banner=False)
+        return qs
+
+
+class BannerCourseAdmin(object):
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums']
+    search_fields = ['name', 'desc', 'detail', 'degree', 'students', 'fav_nums']
+    list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums']
+    ordering = ['-click_nums']
+    readonly_fields = ['fav_nums', 'students']
+    exclude = ['click_nums']
+    inlines = [LessonInline, CourseResourceInline]
+
+    def queryset(self):
+        qs = super(BannerCourseAdmin, self).queryset()
+        qs.filter(is_banner=True)
+        return qs
+
 
 class LessonAdmin(object):
     list_display = ['course', 'name', 'add_time']
     search_fields = ['course', 'name']
     list_filter = ['course__name', 'name', 'add_time']
 
+
 class VideoAdmin(object):
     list_display = ['lesson', 'name', 'add_time']
     search_fields = ['lesson', 'name']
     list_filter = ['lesson', 'name', 'add_time']
+
 
 class CourseResourceAdmin(object):
     list_display = ['course', 'name', 'download', 'add_time']
@@ -28,6 +62,7 @@ class CourseResourceAdmin(object):
     list_filter = ['lesson', 'name', 'download', 'add_time']
 
 xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(BannerCourse, BannerCourseAdmin)
 xadmin.site.register(Lesson, LessonAdmin)
 xadmin.site.register(Video, VideoAdmin)
 xadmin.site.register(CourseResource, CourseResourceAdmin)
